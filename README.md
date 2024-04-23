38eb9f282c883ea2863e68c0897f0b80de6f9683
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-04-23 14:29:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# OpenWRT-CI
+云编译OpenWRT固件
+
+LEDE源码：
+https://github.com/coolsnowwolf/lede
+
+IMMORTALWRT源码：
+https://github.com/immortalwrt/immortalwrt
+
+# 固件简要说明：
+
+固件每天早上4点自动编译。
+
+固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
+
+ROCKCHIP系列、MEDIATEK系列、X64系列。
+
+# 目录简要说明：
+
+Depends.txt——环境依赖列表
+
+workflows——自定义CI配置
+
+Scripts——自定义脚本
+
+Config——自定义配置
---
+++ Scripts/Packages.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#更新软件包
+UPDATE_PACKAGE() {
+	local PKG_NAME=$1
+	local PKG_REPO=$2
+	local PKG_BRANCH=$3
+	local PKG_SPECIAL=$4
+	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
+
+	rm -rf $(find ../feeds/luci/ -type d -iname "*$PKG_NAME*" -prune)
+
+	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+
+	if [[ $PKG_SPECIAL == "pkg" ]]; then
+		cp -rf $(find ./$REPO_NAME/ -type d -iname "*$PKG_NAME*" -prune) ./
+		rm -rf ./$REPO_NAME
+	elif [[ $PKG_SPECIAL == "name" ]]; then
+		mv -f $REPO_NAME $PKG_NAME
+	fi
+}
+
+UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "$([[ $WRT_URL == *"lede"* ]] && echo "main" || echo "js")"
+UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
+UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
+UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
+
+UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
+UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
+
+if [[ $WRT_URL == *"immortalwrt"* ]]; then
+	#UPDATE_PACKAGE "homeproxy" "muink/homeproxy" "dev"
+	UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "dev"
+fi
+
+#更新软件包版本
+UPDATE_VERSION() {
+	local PKG_NAME=$1
+	local NEW_VER=$2
+	local NEW_HASH=$3
+	local PKG_FILE=$(find ../feeds/packages/*/$PKG_NAME/ -type f -name "Makefile" 2>/dev/null)
+
+	if [ -f "$PKG_FILE" ]; then
+		local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" $PKG_FILE)
+		if dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
+			sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" $PKG_FILE
+			sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" $PKG_FILE
+			echo "$PKG_NAME ver has updated!"
+		else
+			echo "$PKG_NAME ver is latest!"
+		fi
+	else
+		echo "$PKG_NAME not found!"
+	fi
+}
+
+UPDATE_VERSION "sing-box" "1.9.0-rc.9" "8bbb1c2650dc7276943a587b1c5f5459434c3523c8bd1077d84457f034633446"
