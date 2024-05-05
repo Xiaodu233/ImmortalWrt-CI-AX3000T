fc7a0501b059cabcacc1a4042cf80e4c87a10782
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-05-05 17:44:33 +0800

    Update Upsing.sh

--- Scripts/Upsing.sh
+++ Scripts/Upsing.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq -r 'first | .tag_name' <<< $(curl --silent https://api.github.com/repos/SagerNet/sing-box/releases)
+version=$(jq -r 'first | .tag_name' <<< $(curl --silent https://api.github.com/repos/SagerNet/sing-box/releases))
