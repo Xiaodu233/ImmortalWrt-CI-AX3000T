ad9bb47a090af27e14d89229bf9cd85149acfe8e
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-05-06 23:12:00 +0800

    Update Upsing.sh

--- Scripts/Upsing.sh
+++ Scripts/Upsing.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-version=$(jq -r 'first | .tag_name' <<< $(curl --silent https://api.github.com/repos/SagerNet/sing-box/releases))
+singver=$(jq -r 'first | .tag_name' <<< $(curl --silent https://api.github.com/repos/SagerNet/sing-box/releases))
+
+singer=$(echo $singver | cut -c2)
+
+wget -P ./ https://codeload.github.com/SagerNet/sing-box/tar.gz/v$singer
+
+singsha=$(sha256sum ./sing-box-$singer.tar.gz)
+
+rm ./sing-box-$singer.tar.gz
+
+fullver=$(UPDATE_VERSION "sing-box" "v$singer" "$singsha")
+
+sed -i '/^UPDATE_VERSION "sing-box" */c'$fullver'' ./Scripts/Packages.sh
