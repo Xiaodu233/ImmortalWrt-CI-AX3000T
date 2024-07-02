6dbd12c433584830d32ad1c65954c31044fa8ac2
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-07-02 18:34:58 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,48 +1,33 @@
 #!/bin/bash
 
+PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
-	HP_PATCH="homeproxy/root/etc/homeproxy/resources"
-	rm -rf ./$HP_PATCH/*
+	HP_RULES="surge"
+	HP_PATCH="homeproxy/root/etc/homeproxy"
 
-	UPDATE_RESOURCES() {
-		local RES_TYPE=$1
-		local RES_FILE=$2
-		local RES_EXT=${2##*.}
-		local RES_REPO=$3
-		local RES_BRANCH=$4
-		local RES_DEPTH=${5:-1}
+	chmod +x ./$HP_PATCH/scripts/*
+	rm -rf ./$HP_PATCH/resources/*
 
-		git clone -q --depth=$RES_DEPTH --single-branch --branch $RES_BRANCH "https://github.com/$RES_REPO.git" ./$RES_TYPE/
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
+	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
-		cd ./$RES_TYPE/
+	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
 
-		echo $(git log -1 --pretty=format:'%s' -- $RES_FILE | grep -o "[0-9]*") > "$RES_TYPE.ver"
-		[ "$RES_EXT" != "db" ] && mv -f "$RES_FILE" "$RES_TYPE.$RES_EXT"
-		cp -f $RES_TYPE.{$RES_EXT,ver} ../$HP_PATCH/ && chmod +x ../$HP_PATCH/*
+	cd .. && rm -rf ./$HP_RULES/
 
-		cd .. && rm -rf ./$RES_TYPE/
-
-		echo "$RES_TYPE done!"
-	}
-
-	UPDATE_RESOURCES "china_ip4" "ipv4.txt" "1715173329/IPCIDR-CHINA" "master" "5"
-	UPDATE_RESOURCES "china_ip6" "ipv6.txt" "1715173329/IPCIDR-CHINA" "master" "5"
-	UPDATE_RESOURCES "gfw_list" "gfw.txt" "Loyalsoldier/v2ray-rules-dat" "release"
-	UPDATE_RESOURCES "china_list" "direct-list.txt" "Loyalsoldier/v2ray-rules-dat" "release"
-	#UPDATE_RESOURCES "geoip" "geoip.db" "1715173329/sing-geoip" "release"
-	#UPDATE_RESOURCES "geosite" "geosite.db" "1715173329/sing-geosite" "release"
-
-	sed -i -e "s/full://g" -e "/:/d" ./$HP_PATCH/china_list.txt
-
-	echo "homeproxy date has been updated!"
+	cd $PKG_PATCH && echo "homeproxy date has been updated!"
 fi
 
 #预置OpenClash内核和数据
-if [ -d *"OpenClash"* ]; then
+if [ -d *"openclash"* ]; then
 	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
 	CORE_TYPE=$(echo $WRT_TARGET | egrep -iq "64|86" && echo "amd64" || echo "arm64")
-	CORE_TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+	CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
 	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
@@ -52,19 +37,36 @@ if [ -d *"OpenClash"* ]; then
 	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
 	GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
 
-	cd ./OpenClash/luci-app-openclash/root/etc/openclash/
+	cd ./luci-app-openclash/root/etc/openclash/
 
-	curl -sfL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
-	curl -sfL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
-	curl -sfL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
+	curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
+	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
+	curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
 
 	mkdir ./core/ && cd ./core/
 
-	curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
-	curl -sfL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
-	curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
+	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
+	curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
+	curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
 
-	chmod +x ./clash* && rm -rf ./*.gz
+	chmod +x ./* && rm -rf ./*.gz
 
-	echo "openclash date has been updated!"
+	cd $PKG_PATCH && echo "openclash date has been updated!"
+fi
+
+#移除ShadowsocksR组件
+PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
+if [ -f "$PW_FILE" ]; then
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
+	sed -i '/ShadowsocksR/d' $PW_FILE
+
+	cd $PKG_PATCH && echo "passwall has been fixed!"
+fi
+
+SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
+if [ -f "$SP_FILE" ]; then
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default y if i386||x86_64||arm/d' $SP_FILE
+	sed -i '/ShadowsocksR/d' $SP_FILE
+
+	cd $PKG_PATCH && echo "ssr-plus has been fixed!"
 fi
