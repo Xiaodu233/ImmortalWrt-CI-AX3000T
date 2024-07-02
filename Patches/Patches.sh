b8585e889219be02325a1f9fcdeb3ca867ab2e5b
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-07-02 18:42:59 +0800

    Update Patches.sh

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#加回部分设备支持
 if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
-	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
-	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
+	mv -f ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
+	patch -R -p1 -d ./wrt < ./Patches/mediatek/mtk*.patch
 
 	echo "$WRT_TARGET patch has been installed!"
+fi
+
+if [[ $WRT_URL == *"lede"* ]]; then
+	sed -i "/#src/d" ./wrt/feeds.conf.default
+	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
+
+	echo "$WRT_URL patch has been installed!"
 fi
