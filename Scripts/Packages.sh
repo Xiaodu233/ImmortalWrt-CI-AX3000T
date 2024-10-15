b8a0e719741e115dee499d7e451f4978a4053319
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2024-10-15 20:03:01 +0800

    sync up

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -35,7 +35,7 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/luci-app-wolplus" "main"
 
-if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
+if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
 
