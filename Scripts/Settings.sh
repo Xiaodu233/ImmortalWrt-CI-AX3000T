1b5c98a48047313837527589da22cc2dfcc11c6a
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2025-01-04 19:53:43 +0800

    syn up

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -45,3 +45,9 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
 fi
+
+#编译器优化
+if [[ $WRT_TARGET != *"X86"* ]]; then
+	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
+	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+fi
