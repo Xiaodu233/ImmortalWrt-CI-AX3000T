1a9a383d0cf715d524505ccf8dc477427af9e9aa
Xiaodu233 <88236905+Xiaodu233@users.noreply.github.com>
2025-01-05 11:26:42 +0800

    test

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,5 +49,5 @@ fi
 #编译器优化
 if [[ $WRT_TARGET != *"X86"* ]]; then
 	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
-	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=1004kc+crypto+crc -mcpu=1004kc+crypto+crc -mtune=mipsel_24kc\"" >> ./.config
 fi
